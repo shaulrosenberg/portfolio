2b4860e5edf110458195be62e74bd0ef35171ac9
shaulrosenberg <shaulr10@gmail.com>
2024-10-23 13:09:21 +0300

    template for my portfolio

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter, Calistoga } from 'next/font/google';
+import "./globals.css";
+
+const inter = Inter({ subsets: ['latin'], variable: "--font-sans" })
+const calistoga = Calistoga({ subsets: ['latin'], variable: "--font-serif", weight: ["400"] })
+
+export const metadata: Metadata = {
+  title: "My Portfolio",
+  description: "Coffee rights Reserved :)",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="bg-gray-900 text-white antialiased">{children}</body>
+    </html>
+  );
+}
