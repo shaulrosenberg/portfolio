8db4046eba9077a029628d09f13bdbac0445a757
shaulrosenberg <shaulr10@gmail.com>
2024-11-20 15:20:39 +0200

    feat: add tape section to main page

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,34 @@
+import StarIcon from "@/assets/icons/star.svg";
+
+const words = [
+  "Performant",
+  "Accessible",
+  "Secure",
+  "Interactive",
+  "Scalable",
+  "User Friendly",
+  "Responsive",
+  "Maintainable",
+  "Search Optimized",
+  "Useable",
+  "Reliable",
+];
+
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
+  return (
+    <div className="py-16 lg:py-24 overflow-x-clip">
+      <div className="bg-gradient-to-r from-emerald-300 to-sky-400 -rotate-3 -mx-1">
+        <div className="flex [mask-image:linear-gradient(to_right, _transparent, _black_10%, _black_90%, _transparent)]">
+          <div className="flex flex-none gap-4 py-3">
+            {words.map((word) => (
+              <div className="inline-flex gap-4 items-center" key={word}>
+                <span className="text-gray-900 uppercase font-extrabold text-sm">{word}</span>
+                <StarIcon className="size-6 text-gray-900 -rotate-12" />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
